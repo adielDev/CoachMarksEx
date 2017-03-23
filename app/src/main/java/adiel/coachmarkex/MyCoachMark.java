6284e405b7b2e052cb5c03431c085c9da955b306
adiel <adielbot@outlook.com>
2017-03-23 18:11:45 +0200

    sec

---
+++ app/src/main/java/adiel/coachmarkex/MyCoachMark.java
@@ -0,0 +1,13 @@
+package adiel.coachmarkex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyCoachMark extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_coach_mark);
+    }
+}
